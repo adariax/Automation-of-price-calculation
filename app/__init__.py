b81f0a96177b0af6e0d03acd51645dc5bbf000c4
Daxa <apelsinKDA@gmail.com>
2020-12-08 23:40:09 +0700

    alpha material+(s) api

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_restful import Api
 
 from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+
 
 db = SQLAlchemy(app)
 
@@ -17,11 +17,8 @@ db.create_all()
 def get_db_session() -> db.Session:
     return db.session
 
-login_manager = LoginManager(app)
 
-# from app.data import posts_api, users_api, posts_callback
+from app.api import material
 
-# api = Api(app)
-# api.add_resource(users_api.UsersResource, '')
-
-# app.register_blueprint(users_api.blueprint)
+api = Api(app)
+api.add_resource(material.MaterialResource, '/api/material/<int:m_id>')
